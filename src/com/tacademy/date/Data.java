63d14343b62edd32851e24c5fb95df7caa5164c8
LeeYoHan <o_o950306@naver.com>
2017-07-21 09:26:47 +0900

    name add

--- src/com/tacademy/date/Data.java
+++ src/com/tacademy/date/Data.java
@@ -1,16 +1,22 @@
 package com.tacademy.date;
 
 public class Data {
+	private String name;
 
-	public Data() {
-		// TODO Auto-generated constructor stub
-	
-	
+	public String getName() {
+		return name;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Data();
+	public void setName(String name) {
+		this.name = name;
 	}
 
+	@Override
+	public String toString() {
+		return "Data [name=" + name + "]";
+	} 
+
+
+	
+	
 }
