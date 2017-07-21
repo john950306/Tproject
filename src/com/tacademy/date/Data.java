14d13d895498fc9a8b1550528e212a42b9bea966
LeeYoHan <o_o950306@naver.com>
2017-07-21 09:21:57 +0900

    first commit

---
+++ src/com/tacademy/date/Data.java
@@ -0,0 +1,16 @@
+package com.tacademy.date;
+
+public class Data {
+
+	public Data() {
+		// TODO Auto-generated constructor stub
+	
+	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Data();
+	}
+
+}
